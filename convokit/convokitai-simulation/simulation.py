199ce292cb262630ffe5ebc01e0d0d5b5dd73ade
Claire Okamoto <134355428+ClaireOka@users.noreply.github.com>
2026-09-26 00:14:58 -0400

    sim_yaml as import

--- convokit/convokitai-simulation/simulation.py
+++ convokit/convokitai-simulation/simulation.py
@@ -1,10 +1,10 @@
 from dl_container import BackendContainer, FIRESTORE_PORT
 from local_backend import LocalBackend
 from create_simulation import _seed_gemini_api_key, create_simulation
+import yaml
 
-
-def simulate(gemini_api_key):
+def simulate(gemini_api_key, sim_yaml: yaml):
     with BackendContainer():
         with LocalBackend(".", reuse_running=True, firestore_port=FIRESTORE_PORT) as backend:
             _seed_gemini_api_key(backend, gemini_api_key)
-            return create_simulation(backend, None)
+            return create_simulation(backend, sim_yaml)
